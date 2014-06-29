06a0784c58345b71a7884f2b8c07a2341cc9fe21
anonymous <viktoryamineva@gmail.com>
2014-06-29 23:05:30 +0300

    Commit my homework

---
+++ T4Template/CodeGenerator.cs
@@ -0,0 +1,9 @@
+﻿// This is generated code:
+class MyGeneratedClass {
+  // Generated code:
+  private int firstProperty = 0;
+  // Generated code:
+  private int secondProperty = 0;
+  // Generated code:
+  private int thirdProperty = 0;
+}
